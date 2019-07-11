373bbb1ca80519105237e9e849baffe2d08d2dcd
Shiva Kandagatla <iamshivakandagatla@gmail.com>
2019-07-11 10:25:41 +0530

    Added comments

--- Leaning _Tower_of_Pisa.py
+++ Leaning _Tower_of_Pisa.py
@@ -1,12 +1,12 @@
 n = int(input("Enter Height : "))
-sp=" "
-st="*"
+sp,st=" ","*"
 if n==1:
     print(st)
 else:
-    temp = n
+    # For Line 1
     print(sp*(n-1) , end="")
     print(st*n)
+    # For All inbetween Lines
     x=n-2
     for i in range(n-2):
         print(sp*x,end="")
@@ -14,5 +14,6 @@ else:
         print(sp*(n-2) , end="")
         print(st)
         x=x-1
+    # For Last Line
     print(st*(n) , end="\n")    
 
